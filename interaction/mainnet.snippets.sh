c4e0fc8d6d72c29c1a21fd494acfa24aab34ba0f
Mihai Daniel Eremia <me@dani.ro>
2023-12-23 21:33:41 +0200

    deploy mainnet

--- interaction/mainnet.snippets.sh
+++ interaction/mainnet.snippets.sh
@@ -1,20 +1,20 @@
 ADDRESS=erd1qqqqqqqqqqqqqpgq7ths5vnfht5muptkxw887jnymvuempn0ah0s7vtrmt
 
-PROXY=https://devnet-api.multiversx.com
+PROXY=https://api.multiversx.com
 PROJECT="${PWD}/output/inscriptions.wasm"
 
 deploy() {
     echo ${PROJECT}
     mxpy --verbose contract deploy --metadata-payable --bytecode=${PROJECT} --recall-nonce  --gas-limit=50000000 \
-    --send --recall-nonce --ledger --ledger-account-index=0 --ledger-address-index=0 --proxy=${PROXY} --chain="D" || return
+    --send --recall-nonce --ledger --ledger-account-index=0 --ledger-address-index=0 --proxy=${PROXY} --chain=1 || return
 }
 
 upgrade() {
     mxpy --verbose contract upgrade ${ADDRESS} --metadata-payable --bytecode=${PROJECT} --recall-nonce --recall-nonce --ledger --ledger-account-index=0 --ledger-address-index=0 \
-    --gas-limit=50000000 --send --outfile="upgrade.json" --proxy=${PROXY} --chain="D" || return
+    --gas-limit=50000000 --send --outfile="upgrade.json" --proxy=${PROXY} --chain=1 || return
 }
 
 issue() {
     mxpy --verbose contract call ${ADDRESS} --function="issue" --value 50000000000000000 --arguments str:Inscriptions str:INS --recall-nonce --ledger --ledger-account-index=0 --ledger-address-index=0 \
-    --gas-limit=90000000 --send --proxy=${PROXY} --chain="D" || return
+    --gas-limit=90000000 --send --proxy=${PROXY} --chain=1 || return
 }
